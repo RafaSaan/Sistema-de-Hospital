6bd427d1f10f1a119781b49a7dfdb5409e4503d4
Rafa San <rafasaanchez@icloud.com>
2021-09-12 00:22:51 -0500

    Renderizado condicional del primer select

--- src/Components/FormGetData.js
+++ src/Components/FormGetData.js
@@ -1,31 +1,32 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import DataInputs from "./DataInputs";
 import "./FormGetData.css";
-import NamesInput from "./NamesInput";
+// import NamesInput from "./NamesInput";
 /*Use el use effect para renderizar inputs nombres de pacientes, meter estos nombres en un variable de estado y con esa variable usar un map para renderizar inputos de datos y actulizar estos datos en el estado para pintarlos despues */
-
+let numberPacients = [];
 const FormGetData = () => {
   const [initalPacient, setInitalPacient] = useState({});
-  const [numberPacients, setNumberPacients] = useState([]);
+  const [dataPacient, setDataPacient] = useState({});
+
   const handleChange = e => {
     setInitalPacient({
       ...initalPacient,
       [e.target.name]: e.target.value,
     });
   };
+  const handleChangeDataPacients = e => {
+    setDataPacient({
+      ...dataPacient,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  console.log(`numero de pacientes ${initalPacient.number}`);
+  let numPacientP = parseInt(initalPacient.number);
 
-  useEffect(() => {
-    for (let i = 1; i <= parseInt(initalPacient.number); i++) {
-      numberPacients.push.apply([i]);
-      // console.log(initalPacient);
-      return numberPacients;
-    }
-  }, [initalPacient]);
-
-  // console.log(numberPacients.push());
-  console.log(numberPacients);
-  console.log(numberPacients.length);
+  if (isNaN(numPacientP)) {
+  } else {
+    numberPacients = Array(numPacientP).fill(1);
+  }
 
   return (
     <div className="hospital__form">
@@ -38,20 +39,29 @@ const FormGetData = () => {
             value={initalPacient.name}
             className="data_select"
             onChange={handleChange}
+            disabled={numberPacients.length === 0 ? false : true}
           >
-            <option value="">---</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
+            <option value={0}>---</option>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
           </select>
         </div>
-        {/* {numberPacients.length === 0 ? (
-          <div> Ingresa el numero de pacientes</div>
+        {/* {numberPacients.length >= 1 && <div>DATAAAAAAAAAAAAAA</div>} */}
+        {numberPacients.length === 0 ? (
+          <div className="messge-empty"> Ingresa el numero de pacientes</div>
         ) : (
-          numberPacients.map(() => <NamesInput />)
-        )} */}
+          numberPacients.map((el, i) => (
+            <DataInputs
+              key={i + 0.26}
+              i={i}
+              handleChangeDataPacients={handleChangeDataPacients}
+              dataPacient={dataPacient}
+            />
+          ))
+        )}
       </form>
     </div>
   );
